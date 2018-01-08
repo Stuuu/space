5008906f2b213969d49b169cda033ac3e9a61f94
Stuart <stuartschrick@gmail.com>
2018-01-07 21:10:10 -0500

    Create tickate.php

---
+++ tickate.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<head>
+<script>
+window.open("http://104.236.54.72/tickate.php", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=0, left=0, width=220, height=205");
+setTimeout(function(){window.location.href="/"},5900);
+</script>
